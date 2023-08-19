9006e6707cd96d75b83bf84b429a81e0347449c6
Mootor <mmootor@ip-0A125213.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2023-08-19 02:37:14 -0700

    fix lintr

--- install_bpg.R
+++ install_bpg.R
@@ -4,14 +4,14 @@ parser <- ArgumentParser();
 parser$add_argument(
     '-av',
     '--add-version',
-    help='Specify version of BPG to download'
+    help = 'Specify version of BPG to download'
     );
 
 args <- parser$parse_args();
 
 bpg.version <- args$add_version;
 
-if(!(startsWith(bpg.version, 'v'))) {
+if( !(startsWith(bpg.version, 'v')) ) {
     bpg.version <- paste('v', bpg.version, sep = '')
     };
 
